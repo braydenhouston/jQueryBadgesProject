a069b9236c1b2c3094d5f788bbb1941e01c0c614
Brayden Houston <bhouston@spillman.com>
2017-03-02 10:37:43 -0700

    Finished project

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,20 @@
 $(function() {
 
   // your code will go here
+  $.ajax({
+    url: 'https://www.codeschool.com/users/2506500.json',
+    dataType: 'jsonp',
+    success: function(response) {
+      // handle response
+          for(var i = 0, len = response.courses.completed.length; i < len; i++){
+               console.log(response.courses.completed[i]);
+               $("#badges").append("<div class='course'>" +
+               "<h3>" + response.courses.completed[i].title + "</h3>" +
+               "<img src='" + response.courses.completed[i].badge + "'>" +
+               "<a class='btn btn-primary' target='_blank' href='" + response.courses.completed[i].url + "'>See Course</a>" +
+               "</div>");
+          }
 
+    }
+  });
 });
